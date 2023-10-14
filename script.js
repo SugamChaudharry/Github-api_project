a2f8af46c5a7e276e9a50d17fcd42708e6274164
SugamChaudharry <nextgenachieverss@gmail.com>
2023-10-14 10:28:09 +0530

    a

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const showBtnEl = document.getElementById("showBtn");
 const coverBody = document.querySelector("#cover");
 const userInput  = document.querySelector('#userNameInput');
+const avatarImgEl = document.querySelector("#avtarImg");
+
+
+
 
 showBtnEl.addEventListener('click',()=>{
     const user = userInput.value;
@@ -11,14 +15,17 @@ function apirequest(user) {
     const xhr = new XMLHttpRequest();
     let requestUrl = `https://api.github.com/users/${user}`
     console.log(requestUrl);
-
+    
     xhr.open('GET', requestUrl)
     xhr.onreadystatechange = function DATAsss() {
         if (xhr.readyState === 4) {
             const data = JSON.parse(this.responseText)
             checking(data)    
-            }
+            const avtarUrl = data.avatar_url;
+            avatarImgEl.setAttribute("src", `${avtarUrl}`);
         }
+    }
+    // avatarImgEl.setAttribute("src", `${data.avatar_url}`);   
     xhr.send();
         
     
@@ -27,6 +34,7 @@ function checking(subject) {
     if(subject.message === "Not Found"){
         alert("enter valid name")
     }else{
-        console.log("👍👍👍👍")
+        coverBody.style.zIndex = "-1"
     }
 }
+
